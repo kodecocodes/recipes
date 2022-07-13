536b4d3f7164510c987d2481b7e6b74e2588029b
Alejandro Saez <alejandrosaezc@gmail.com>
2022-07-13 20:04:17 +0200

    Card Comp created & Basic Map response

--- front-recipes/src/pages/HomePage.jsx
+++ front-recipes/src/pages/HomePage.jsx
@@ -1,36 +1,41 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Button, Box, Text } from "@chakra-ui/react";
 import { getAllRecipes } from "../services/recipeService";
+import Card from "../components/Card";
 
 export const HomePage = () => {
-  function sayHello() {
-    getAllRecipes(1, 1, "").then((response) => {
-      if (
-        response.status === 200 &&
-        response.data.data &&
-        response.data.totalPages &&
-        response.data.currentPage
-      ) {
-        let { totalPages, currentPage, data } = response.data;
-        alert(data[0].name);
-      } else {
-        throw new Error(`Error obtaining recipes: ${response.data}`);
-      }
-    });
-    //alert("Hello, World!");
-  }
+  // State of the component
+  const [recipes, setRecipes] = useState();
+  const [hasLoaded, setHasLoaded] = useState();
 
-  return (
-    <Button
-      fontSize="sm"
-      variant="brand"
-      fontWeight="500"
-      w="100%"
-      h="50"
-      mb="24px"
-      onClick={sayHello}
-    >
-      Click me!
-    </Button>
+  useEffect(() => {
+    getAllRecipes(3, 1, "")
+      .then((response) => {
+        if (
+          response.status === 200 &&
+          response.data.data &&
+          response.data.totalPages &&
+          response.data.currentPage
+        ) {
+          let { totalPages, currentPage, data } = response.data;
+          setRecipes(data);
+          setHasLoaded(true);
+        } else {
+          throw new Error(`Error obtaining recipes: ${response.data}`);
+        }
+      })
+      .catch((error) => console.error(`[GET ALL RECIPES ERROR]: ${error}`));
+  }, []);
+
+  return hasLoaded ? (
+    <Card mb={{ base: "0px", lg: "20px" }} align="center">
+      {recipes.map((recipe, key) => (
+        <Text key={key} color="blue" fontSize="xl" fontWeight="1000">
+          {recipe.name}
+        </Text>
+      ))}
+    </Card>
+  ) : (
+    <Text color="blue" fontSize="xl" fontWeight="1000"></Text>
   );
 };
