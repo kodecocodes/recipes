54f25afb8a867f4c665996a630d6993867a5f36f
Alejandro <alejandrosaezc@gmail.com>
2022-07-17 19:09:03 +0200

    Back Update. Get Ingredients by Type

--- back-recipes/src/controller/IngredientsController.js
+++ back-recipes/src/controller/IngredientsController.js
@@ -6,10 +6,25 @@ export class IngredientsController extends AppController {
   constructor(model) {
     super(model);
   }
-  async getIngredients() {
+  async getIngredients(types) {
     let response = [];
     LogSuccess(" [/api/ingredients] Get Request");
     let recipes = await Normalize();
+    //We can get ingredients by Types
+    if (types) {
+      recipes.forEach((element) => {
+        element.ingredients.forEach((ingredient) => {
+          for (let type of types.split(",")) {
+            if (ingredient.type == type) {
+              response.push(ingredient.name);
+            }
+          }
+        });
+      });
+      response = new Set(response);
+      return [...response];
+    }
+    //If no type selected we get all ingredients
     recipes.forEach((element) => {
       element.ingredients.forEach((ingredient) => {
         response.push(ingredient.name);
--- back-recipes/src/routes/IngredientsRouter.js
+++ back-recipes/src/routes/IngredientsRouter.js
@@ -9,10 +9,12 @@ ingredientsRouter
   .route("/")
   // GET:
   .get(async (req, res) => {
+    //Obtain Query params
+    let types = req.query.types;
     // Send to the client the response
     const controller = new IngredientsController();
     // Obtain Response
-    const response = await controller.getIngredients();
+    const response = await controller.getIngredients(types);
     // Send to the client the response
     return res.send(response);
   });
